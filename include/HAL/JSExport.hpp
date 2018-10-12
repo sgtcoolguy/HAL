4f446d7490989dddeb5fcbccf79bb347837fc131
Kota Iguchi <developer@infosia.co.jp>
2018-10-12 12:45:18 +0900

    Make sure to catch exception in callbacks

--- include/HAL/JSExport.hpp
+++ include/HAL/JSExport.hpp
@@ -56,14 +56,24 @@ namespace HAL {
 				t = static_cast<JSObject>(function_object.GetProperty("__constructor")).GetPrivate<T>();
 			}
 
-			if (t) {
-				return callback(*t, arguments, this_object);
-			}
-
-			// If we still can't find the parent, we just throw an error.
 			const auto js_context = this_object.get_context();
 			const auto undefined = js_context.CreateUndefined();
 
+			if (t) {
+				try {
+					return callback(*t, arguments, this_object);
+				} catch (const std::runtime_error& ex) {
+					const auto error = js_context.CreateError(ex.what());
+					JsSetException(static_cast<JsValueRef>(error));
+					return undefined;
+				} catch (...) {
+					const auto error = js_context.CreateError("Unknown error at " + name);
+					JsSetException(static_cast<JsValueRef>(error));
+					return undefined;
+				}
+			}
+
+			// If we still can't find the parent, we just throw an error.
 			const auto error = js_context.CreateError("Callback for the function " + name + " is not found.");
 			JsSetException(static_cast<JsValueRef>(error));
 
@@ -74,17 +84,44 @@ namespace HAL {
 	template<typename T>
 	void JSExport<T>::AddValueProperty(const std::string& name, GetNamedValuePropertyCallback<T> getter, SetNamedValuePropertyCallback<T> setter) {
 		js_class__.AddValueProperty(name,
-			[getter](JSObject this_object) {
+			[name, getter](JSObject this_object) {
+
+			const auto js_context = this_object.get_context();
+			const auto undefined = js_context.CreateUndefined();
+
 			auto t = this_object.GetPrivate<T>();
 			if (t && getter) {
-				return getter(*t);
+				try {
+					return getter(*t);
+				} catch (const std::runtime_error& ex) {
+					const auto error = js_context.CreateError(ex.what());
+					JsSetException(static_cast<JsValueRef>(error));
+					return undefined;
+				} catch (...) {
+					const auto error = js_context.CreateError("Unknown error at " + name);
+					JsSetException(static_cast<JsValueRef>(error));
+					return undefined;
+				}
 			}
-			return this_object.get_context().CreateUndefined();
+			return undefined;
 		},
-			[setter](JSObject this_object, const JSValue& value) {
+			[name, setter](JSObject this_object, const JSValue& value) {
+
 			auto t = this_object.GetPrivate<T>();
 			if (t && setter) {
-				return setter(*t, value);
+				try {
+					return setter(*t, value);
+				} catch (const std::runtime_error& ex) {
+					const auto js_context = this_object.get_context();
+					const auto error = js_context.CreateError(ex.what());
+					JsSetException(static_cast<JsValueRef>(error));
+					return false;
+				} catch (...) {
+					const auto js_context = this_object.get_context();
+					const auto error = js_context.CreateError("Unknown error at " + name);
+					JsSetException(static_cast<JsValueRef>(error));
+					return false;
+				}
 			}
 			return false;
 		}
@@ -93,12 +130,26 @@ namespace HAL {
 
 	template<typename T>
 	void JSExport<T>::AddConstantProperty(const std::string& name, GetNamedValuePropertyCallback<T> getter) {
-		js_class__.AddConstantProperty(name, [getter](JSObject this_object) {
+		js_class__.AddConstantProperty(name, [name, getter](JSObject this_object) {
+
+			const auto js_context = this_object.get_context();
+			const auto undefined = js_context.CreateUndefined();
+
 			auto t = this_object.GetPrivate<T>();
 			if (t && getter) {
-				return getter(*t);
+				try {
+					return getter(*t);
+				} catch (const std::runtime_error& ex) {
+					const auto error = js_context.CreateError(ex.what());
+					JsSetException(static_cast<JsValueRef>(error));
+					return undefined;
+				} catch (...) {
+					const auto error = js_context.CreateError("Unknown error at " + name);
+					JsSetException(static_cast<JsValueRef>(error));
+					return undefined;
+				}
 			}
-			return this_object.get_context().CreateUndefined();
+			return undefined;
 		});
 	}
 
