abfbb5feeb0cc5e5b6595ea918684feea3403179
Christopher Williams <chris.a.williams@gmail.com>
2019-02-04 21:41:13 -0500

    build: use pre-compield header (#89)

---
+++ include/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <locale>
+#include <codecvt>
+#include <cstddef>
+#include <vector>
+#include <utility>
+#include <mutex>
+
+#include "HAL/detail/JSBase.hpp"
